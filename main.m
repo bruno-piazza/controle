ba91d6b217d73d4ece57a03176788e0a6de55b8c
gustavo-nr <80063081+gustavo-nr@users.noreply.github.com>
2022-05-17 00:51:25 -0300

    update

--- main.m
+++ main.m
@@ -7,13 +7,18 @@ B = importdata('matrix_B1lin.txt');
 C = importdata('matrix_C1.txt');
 D = importdata('matrix_D1.txt');
 
-p = [-3+1i -3-1i -5+1i -5-1i -3 -4];
+Co = ctrb(A,B);
+Ob = obsv(A,C);
+
+polos = eig(A);
+
+p = [-10 -30 -5 -2 -5 -10];
 K=place(A,B,p);
 sys = ss(A-B*K,B,C,D);
 
 figure(1)
 step(sys)
-
-figure(2)
-impulse(sys)
+% 
+% figure(2)
+% impulse(sys)
 

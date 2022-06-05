7a58906a0638fd7fec5b75b3e4cecefe045f8bfb
gustavo-nr <80063081+gustavo-nr@users.noreply.github.com>
2022-06-05 19:08:06 -0300

    quat

--- quat2eulang.m
+++ quat2eulang.m
@@ -1,5 +1,5 @@
-function y = quat2eulang(q0,q1,q2,q3,seq)
+function [Z1,Y1,Z2] = quat2eulang(q0,q1,q2,q3,seq)
     L = length(q2);
     q = [ones(L,1)*q0,q1,q2,q3];
-    y = quat2eul(q,seq);
+    [Z1,Y1,Z2] = quat2angle(q,seq);
 end
